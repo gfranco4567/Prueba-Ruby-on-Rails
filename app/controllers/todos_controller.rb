fea99a207940c15673b64e69dfbfd067e89d0046
Gustavo Franco <gfranco@arquitecto.com>
2019-01-30 16:20:39 -0300

    prueba-terminada

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,60 @@
 class TodosController < ApplicationController
+# --
+before_action :set_todo, only: %i[show edit update destroy]
+
+  def index
+    @todos = Todo.order(:id)
+    @todo = Todo.new
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to root_path
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to root_path, notice: 'El ToDo fue actualizado!!'
+    else
+      redirect_to root_path, alert: 'Ha ocurrido un error. Por favor intenta nuevamente..'
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to root_path, notice: 'El ToDo fue eliminado!'
+  end
+
+  def set_complete
+    @todo = Todo.find(params[:id])
+    @todo.complete = true
+    @todo.save
+    redirect_to root_path, notice: 'El ToDo ha sido completado!!'
+    end
+
+def completed
+@todos = Todo.where(complete: true)
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+
+# --
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
+  get 'todos', to: 'todos#index'
+  get 'todos/new', as: 'new_todo'
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+  put 'todos/:id', to: 'todos#update'
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
+
+  patch 'todos/:id/set_complete', to: 'todos#set_complete', as:'set_complete'
+  get 'todos/list', as: 'todos_list'
+
+  root 'todos#index'
 end
